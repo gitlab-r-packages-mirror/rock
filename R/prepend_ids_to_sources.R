ba3611298a68254f63107d6f0660809fd385b6b9
matherion <gjalt-jorn@behaviorchange.eu>
2019-04-23 12:04:01 +0200

    All unique UIDs now

--- R/prepend_ids_to_sources.R
+++ R/prepend_ids_to_sources.R
@@ -41,13 +41,12 @@ prepend_ids_to_sources <- function(input,
                             origin=origin,
                             encoding = encoding,
                             silent=TRUE);
+    ### Setting origin to a few seconds in the future to make sure all
+    ### uids are unique
     last_uid <-
-      gsub("^\\[\\[(.*)\\]\\].*$", "\\1", tail(tmp)[1]);
-    print(last_uid);
-    print(base30toNumeric(last_uid));
+      gsub("^\\[\\[(.*)\\]\\].*$", "\\1", tail(tmp, 1));
     origin <-
       as.POSIXct((1+base30toNumeric(last_uid)) / 100, origin="1970-01-01");
-    print(origin);
   }
   if (!silent) {
     message("I just added utterenance identifiers to ", length(rawSourceFiles),
