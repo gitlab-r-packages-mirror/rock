587d20bfebeec1465061dcb8f63f63663cee4902
Bjorn Padding <b.h.padding@gmail.com>
2019-04-29 21:23:26 +0700

    Set code color to #f4d142 and extensions to .rock,.txt

--- iROCK/js/script.js
+++ iROCK/js/script.js
@@ -62,8 +62,9 @@ $(function () {
     $("content").on('DOMNodeInserted', '.line', function (event) {
         $(this).droppable({
             accept: function ($this) {
-                console.log($(this).find('.tags').has('[data-id="' + $this.data('id').replace(/"/,'\\\\"') + '"]').length);
-                return !$(this).find('.tags').has('[data-id="' + $this.data('id').replace(/"/,'\\\\"') + '"]').length;
+                console.log(($this.data('id')));
+                console.log($(this).find('.tags').has('[data-id="' + String($this.data('id') || "").replace(/"/,'\\\\"') + '"]').length);
+                return !$(this).find('.tags').has('[data-id="' + String($this.data('id') || "").replace(/"/,'\\\\"') + '"]').length;
             },
             drop: function (event, ui) {
                 var $this = ui.draggable;
