8c0133528efa9f7b530326b62d9aa1a76c87c21a
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2020-02-28 11:35:18 +0100

    Better attribute table stuff

--- R/show_attribute_table.R
+++ R/show_attribute_table.R
@@ -1,7 +1,12 @@
-show_attribute_table <- function(x) {
-  if (knitr::knit_) {
+show_attribute_table <- function(x,
+                                 output = rock::opts$get(tableOutput),
+                                 tableOutputCSS = ufs::opts$get(tableOutputCSS)) {
+  if (isTRUE(getOption('knitr.in.progress'))) {
     print(knitr::kable(x$attributesDf));
   } else {
+    ufs::exportToHTML(x$attributesDf,
+                      output = output,
+                      tableOutputCSS = tableOutputCSS);
   }
   return(invisible(x));
 }
