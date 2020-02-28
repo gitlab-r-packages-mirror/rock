158644bdd129a81d57a7293ed26c6df310e23f3f
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2020-02-28 11:39:31 +0100

    Updated NAMESPACE

--- R/show_attribute_table.R
+++ R/show_attribute_table.R
@@ -1,3 +1,15 @@
+#' Show a table with all attributes in the RStudio viewer and/or console
+#'
+#' @param x A `rockParsedSources` object (the result of a call to
+#' `rock::parse_sources`).
+#' @param output The output: a character vector with one or more
+#' of "`console`" (the raw concatenated input, without conversion
+#' to HTML), "`viewer`", which uses the RStudio viewer if available,
+#' and one or more filenames in existing directories.
+#' @param tableOutputCSS The CSS to use for the HTML table.
+#'
+#' @return `x`, invisibly.
+#' @export
 show_attribute_table <- function(x,
                                  output = rock::opts$get(tableOutput),
                                  tableOutputCSS = ufs::opts$get(tableOutputCSS)) {
