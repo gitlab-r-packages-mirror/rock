f69f0cdee77f55e8054d428b36dbc947612da956
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2025-03-15 09:09:16 +0100

    added more files

---
+++ R/export_ROCKproject.R
@@ -0,0 +1,30 @@
+export_ROCKproject <- function(path,
+                               output,
+                               includeRegex = NULL,
+                               excludeRegex = NULL,
+                               createDirs = FALSE) {
+
+  outputDir <- dirname(output);
+
+  if (!dir.exist(dirname(output))) {
+    if (createDirs) {
+      dir.create(outputDir, recursive = TRUE);
+    } else {
+      stop("The directory where you wanted to create the ROCKproject file, `",
+           outputDir,
+           "`, does not exist!");
+    }
+  }
+
+  if (!dir.exists(path)) {
+
+    stop("The directory you specified to export the ROCKproject from, `", path,
+         "`, does not exist!");
+
+  }
+
+
+  zip(zipfile <-
+
+
+}
