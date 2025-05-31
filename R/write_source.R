082a847afe1f3a13d5e092e8ac7f13a65043164a
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2025-05-31 21:09:19 +0200

    uncommented example with pipe

--- R/write_source.R
+++ R/write_source.R
@@ -41,16 +41,18 @@
 #' );
 #'
 #' ### From R 4.1 onwards, you can also chain
-#' ### these commands using the pipe operator:
-#' if (R.Version()$minor >= "4.1") {
-#'   loadedSource <-
+#' ### these commands using the pipe operator.
+#' ###
+#' ### Note that that means that this example
+#' ### will not run if you have a previous
+#' ### version of R.
+#' loadedSource <-
 #'
-#'     rock::load_source(exampleFile) |>
+#'   rock::load_source(exampleFile) |>
 #'
-#'     rock::code_source(c("Lorem Ipsum" = "lorumIpsum")) |>
+#'   rock::code_source(c("Lorem Ipsum" = "lorumIpsum")) |>
 #'
-#'     rock::write_source(tempFile);
-#' }
+#'   rock::write_source(tempFile);
 #'
 #' @export
 write_source <- function(x,
